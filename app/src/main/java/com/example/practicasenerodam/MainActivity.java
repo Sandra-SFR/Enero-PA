98ab71d385972effb5d2222549230129c2674286
Sandra-SFR <sandrafantonirubio@gmail.com>
2023-01-13 13:59:06 +0100

    guardado para comer

--- app/src/main/java/com/example/practicasenerodam/MainActivity.java
+++ app/src/main/java/com/example/practicasenerodam/MainActivity.java
@@ -1,12 +1,24 @@
 package com.example.practicasenerodam;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static com.example.practicasenerodam.db.Constants.DATABASE_NAME;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.example.practicasenerodam.adapter.TareaAdapter;
+import com.example.practicasenerodam.db.AppDatabase;
 import com.example.practicasenerodam.domain.Tarea;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,5 +30,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        taskList = new ArrayList<>();
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new TareaAdapter(this, taskList);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        final AppDatabase db = Room.databaseBuilder(this, AppDatabase.class, DATABASE_NAME)
+                .allowMainThreadQueries().build();
+        taskList.clear();
+        taskList.addAll(db.tareaDao().getAll());
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actionbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.register_task) {
+            Intent intent = new Intent(this, RegistrarTareaActivity.class);
+            startActivity(intent);
+            return true;
+        } else if (item.getItemId() == R.id.view_map) {
+            Intent intent = new Intent(this, MapaActivity.class);
+            startActivity(intent);
+        }
+
+        return false;
+    }
+
+    public void continueButton(View view) {
+
     }
 }
--- app/src/main/java/com/example/practicasenerodam/MapaActivity.java
+++ app/src/main/java/com/example/practicasenerodam/MapaActivity.java
@@ -1,14 +1,34 @@
 package com.example.practicasenerodam;
 
+import static com.example.practicasenerodam.db.Constants.DATABASE_NAME;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.os.Bundle;
 
+import com.example.practicasenerodam.db.AppDatabase;
+import com.mapbox.maps.MapView;
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotationManager;
+
+import java.util.List;
+
 public class MapaActivity extends AppCompatActivity {
+
+    private MapView mapView;
+    private PointAnnotationManager pointAnnotationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapa);
+
+        mapView = findViewById(R.id.mapView);
+        initializePointManager();
+
+        final AppDatabase db = Room.databaseBuilder(this, AppDatabase.class, DATABASE_NAME)
+                .allowMainThreadQueries().build();
+        List<Tarea> tasks = db.tareaDao().getAll();
+        addTasksToMap(tasks);
     }
 }
